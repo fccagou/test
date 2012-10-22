0ee885f6c412f6bab9308d9aaa686342a611cff9
François <francois@efate>
2012-10-22 20:54:37 +0200

    Change configuration file.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='MyFirstApp',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='First OpenShift App',
+      author='François Chenais',
+      author_email='Francois.Chenais@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
