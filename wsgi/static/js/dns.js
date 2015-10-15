1f7a2661f373a8086e5e09c4f31900296db86581
fccagou <fccagou@gmail.com>
2015-10-15 21:44:36 +0200

    add: iscagoualive function

--- wsgi/static/js/dns.js
+++ wsgi/static/js/dns.js
@@ -1,6 +1,41 @@
 
+function isCagouAlive() {
 
+    var req = new XMLHttpRequest();
+    req.open('GET', '/iscagoualive' , false);
+    req.send();
 
+    if ( req.responseText == '1' ) {
+	return dns_gethostbyname('lenidducagou')
+    }
+
+    return ''
+
+}
+
+function isCagouAlive2() {
+
+	var response="";
+	var ip = dns_gethostbyname('lenidducagou');
+
+	if ( ip != "" ) {
+		var req = new XMLHttpRequest();
+		req.open('GET', 'http://'+ip+'/', false);
+		try {
+			req.send();
+			if (req.status == 200 ) {
+				response = ip;
+			} else {
+				response = "";
+			}
+		} catch(err) {
+			// Error because of CrossPlateforme
+			response = ip;
+		}
+		
+	}
+	return response;
+}
 
 
 function dns_gethostbyname(hostname) {
